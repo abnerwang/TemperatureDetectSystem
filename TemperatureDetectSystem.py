8a5429a147aa1c5b63ea428d2b037724af0e385e
abnerwang <abnerwang@yeah.net>
2018-01-20 12:49:55 +0800

    加入未实现的 https

--- TemperatureDetectSystem.py
+++ TemperatureDetectSystem.py
@@ -2,7 +2,7 @@ import os
 
 import pymysql
 from flask_migrate import Migrate, MigrateCommand
-from flask_script import Manager, Shell
+from flask_script import Manager, Shell, Server
 
 from app import create_app, db
 from app.models import User, NoCoImage, CoImage
@@ -19,7 +19,9 @@ def make_shell_context():
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
-
+# HTTPS 暂不实现
+# manager.add_command('runserver', Server('localhost', port=443, ssl_crt="app/cert.pem", ssl_key="app/key.pem"))
+manager.add_command('runserver', Server('localhost', port=8080))
 
 if __name__ == '__main__':
     manager.run()
